26153f8fb5353b37f665b84b2f5db6ed403dc6aa
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-05-18 01:24:55 +0900

    feat: update with sample code

--- app/src/main/java/jp/matsumoto/kou/mytokyubus/MainActivity.java
+++ app/src/main/java/jp/matsumoto/kou/mytokyubus/MainActivity.java
@@ -1,20 +1,18 @@
 package jp.matsumoto.kou.mytokyubus;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements DownloadCallback<String> {
 
     public static final String EXTRA_MESSAGE = "jp.matsumoto.kou.mytokyubus.MESSAGE";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
@@ -23,5 +21,75 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+
+    // Keep a reference to the NetworkFragment, which owns the AsyncTask object
+    // that is used to execute network ops.
+    private NetworkFragment mNetworkFragment;
+
+    // Boolean telling us whether a download is in progress, so we don't trigger overlapping
+    // downloads with consecutive button clicks.
+    private boolean mDownloading = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNetworkFragment = NetworkFragment.getInstance(getFragmentManager(), "https://www.google.com");
+    }
+
+    private void startDownload() {
+        if (!mDownloading && mNetworkFragment != null) {
+            // Execute the async download.
+            mNetworkFragment.startDownload();
+            mDownloading = true;
+        }
+    }
+
+
+    @Override
+    public void updateFromDownload(String result) {
+        // Update your UI here based on result of download.
+    }
+
+    @Override
+    public NetworkInfo getActiveNetworkInfo() {
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo;
+    }
+
+    @Override
+    public void onProgressUpdate(int progressCode, int percentComplete) {
+        switch (progressCode) {
+            // You can add UI behavior for progress update here.
+            case Progress.Error:
+                //
+                break;
+            case Progress.CONNECT_SUCCESS:
+                //
+                break;
+            case Progress.GET_INPUT_STREAM_SUCCESS:
+                //
+                break;
+            case Progress.PROCESS_INPUT_STREAM_IN_PROGRESS:
+                //
+                break;
+            case Progress.PROCESS_INPUT_STREAM_SUCCESS:
+                //
+                break;
+        }
+    }
+
+    @Override
+    public void finishDownloading() {
+        mDownloading = false;
+        if (mNetworkFragment != null) {
+            mNetworkFragment.cancelDownload();
+        }
     }
 }
